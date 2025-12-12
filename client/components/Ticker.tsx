796ad5236c3d0d8856aec9d82158fb4a9f354286
Johncarlo Ababa <mydadisbrave2@gmail.com>
2025-12-12 15:24:49 +0800

    Update Ticker.tsx

--- client/components/Ticker.tsx
+++ client/components/Ticker.tsx
@@ -9,13 +9,13 @@ export default function Ticker() {
   const repeatedText = text.repeat(20);
 
   return (
-    <div className="w-full overflow-visible bg-white py-[81px] pb-[75px] lg:py-8 relative mt-0 md:mt-12 lg:mt-0 mb-[30px]">
+    <div className="w-full overflow-hidden bg-white py-[81px] pb-[75px] lg:py-8 relative mt-0 md:mt-12 lg:mt-0 mb-[30px]">
       <div className="rotate-[-2deg] transform origin-center">
         <div className="flex whitespace-nowrap animate-ticker">
-          <div className="font-inter text-lg font-medium text-black flex items-center gap-4 mb-[29px]">
+          <div className="font-inter text-lg font-medium text-black flex items-center gap-10 mb-[29px]">
             {repeatedText}
           </div>
-          <div className="font-inter text-lg font-medium text-black flex items-center gap-4">
+          <div className="font-inter text-lg font-medium text-black flex items-center gap-10">
             {repeatedText}
           </div>
         </div>
